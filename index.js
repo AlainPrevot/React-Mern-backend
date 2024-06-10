d711db3b101eaa36c9ef77e0f656e19a08533dc1
Alenchu <alain.k.prevot@gmail.com>
2024-06-10 17:46:47 -0300

    Instalando CORS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 require('dotenv').config()
+const cors = reuire('cors')
 const {dbConnection} = require('./database/config')
 
 //Crear el servidor de express
@@ -9,6 +10,8 @@ const app = express();
 
 //Base de datos
 dbConnection()
+//Cors
+app.use(cors())
 
 //Directorio Publico
 app.use(express.static('public'));
